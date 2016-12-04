e3f21384c1df46087c836aada4af48e77830b02a
Bijay Timilsina <bjtimi.007@gmail.com>
2016-12-04 11:42:56 +0100

    coercion example

--- declaration&expression.js
+++ declaration&expression.js
@@ -8,3 +8,5 @@ console.log(x);  // 1. prints the function as a value of x variable (a string li
 console.log(x()); // 2. executes the function due to () and stores the value in x and prints it out
 console.log(y);  // 3. same as 1 but just that the function has name y and is method of window(?) object
 console.log(y()); // 4. same as 2 but stores doesn't store anywhere (is it y = 2016 ?? gotta test it)
+
+console.log(x+z); // here js does implicit coercion -> concatenates x with z (whole function definition with z(value undefined here)
